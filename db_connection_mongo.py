68edae22b6c522c8a4115346e2b5c70d0b84e4ea
Ardavan Sherafat <75146210+ArdavanSh@users.noreply.github.com>
2024-10-17 18:54:46 -0700

    Update db_connection_mongo.py

--- db_connection_mongo.py
+++ db_connection_mongo.py
@@ -31,13 +31,14 @@ def createDocument(documents, docId, docText, docTitle, docDate, docCat):
     wordsCounter = Counter(txt)
 
     # Convert Counter to a list of dictionaries
-    terms_list = [{"term": word, "count": count} for word, count in wordsCounter.items()]
+    terms_list = [{"term": word, "count": count, "num_chars":len(word)} for word, count in wordsCounter.items()]
 
     # Value to be inserted
     doc = {
         "_id": docId,
-        "text": docText,
         "title": docTitle,
+        "text": docText,
+        "num_chars": sum(item["num_chars"] * item["count"] for item in terms_list),
         "date": datetime.datetime.strptime(docDate, "%Y-%m-%d"),
         "category": docCat,
         "terms": terms_list  
@@ -50,10 +51,11 @@ def updateDocument(documents, docId, docText, docTitle, docDate, docCat):
 
     txt = re.sub(f"[{re.escape(string.punctuation)}]", "", docText.lower()).split()
     wordsCounter = Counter(txt)
-    terms_list = [{"term": word, "count": count} for word, count in wordsCounter.items()]
+    terms_list = [{"term": word, "count": count, "num_chars":len(word)} for word, count in wordsCounter.items()]
 
     # documents fields to be updated
-    doc = {"$set": {"text": docText, "title": docTitle, "date":datetime.datetime.strptime(docDate, "%Y-%m-%d"), "category":docCat, "terms":terms_list}}
+    doc = {"$set": {"title": docTitle, "text": docText, "num_chars": sum(item["num_chars"] * item["count"] for item in terms_list),
+                    "date":datetime.datetime.strptime(docDate, "%Y-%m-%d"), "category":docCat, "terms":terms_list}}
 
     # Updating the dpcument
     documents.update_one({"_id": docId}, doc)
@@ -80,87 +82,6 @@ def getIndex(documents):
     sorted_data = [(key, sorted(value)) for key, value in sorted(invertedIndex.items())]
     
     return sorted_data
-
-# _______________________________________________________________________________________
-
-
-
-def createUser(col, id, name, email):
-
-    # Value to be inserted
-    user = {"_id": id,
-            "name": name,
-            "email": email,
-            }
-
-    # Insert the document
-    col.insert_one(user)
-
-def updateUser(col, id, name, email):
-
-    # User fields to be updated
-    user = {"$set": {"name": name, "email": email} }
-
-    # Updating the user
-    col.update_one({"_id": id}, user)
-
-def deleteUser(col, id):
-
-    # Delete the document from the database
-    col.delete_one({"_id": id})
-
-def getUser(col, id):
-
-    user = col.find_one({"_id":id})
-
-    if user:
-        return str(user['_id']) + " | " + user['name'] + " | " + user['email']
-    else:
-        return []
-
-def createComment(col, id_user, dateTime, comment):
-
-    # Comments to be included
-    comments = {"$push": {"comments": {
-                                       "datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S"),
-                                       "comment": comment
-                                       } }}
-
-    # Updating the user document
-    col.update_one({"_id": id_user}, comments)
-
-def updateComment(col, id_user, dateTime, new_comment):
-
-    # User fields to be updated
-    comment = {"$set": {"comments.$.comment": new_comment} }
-
-    # Updating the user
-    col.update_one({"_id": id_user, "comments.datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")}, comment)
-
-def deleteComment(col, id_user, dateTime):
-
-    # Comments to be delete
-    comments = {"$pull": {"comments": {"datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")} }}
-
-    # Updating the user document
-    col.update_one({"_id": id_user}, comments)
-
-def getChat(col):
-
-    # creating a document for each message
-    pipeline = [
-                 {"$unwind": { "path": "$comments" }},
-                 {"$sort": {"comments.datetime": 1}}
-               ]
-
-    comments = col.aggregate(pipeline)
-
-    chat = ""
-
-    for com in comments:
-        chat += com['name'] + " | " + com['comments']['comment'] + " | " + str(com['comments']['datetime']) + "\n"
-
-    return chat
 
 
 
